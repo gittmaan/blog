218cb80c5fd99ee15512e7b3d47bca4ea07365b6
Girish Tryambake <gtryambake@host-154-7-0-10.sfoffice.verticalresponse.com>
2011-11-29 15:31:37 -0800

    controller basic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   respond_to :html, :json, :xml, :js
 
   def index
-    @posts = Post.all.order('posted_at DESC').limit(15)
+    @posts = Post.all.order( 'posted_at DESC' ).limit( 15 )
     respond_with @posts
   end
 
@@ -13,26 +13,38 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(@post_id)
+    @post = Post.find( @post_id )
     @comment = @post.comments.build
   end
 
   def edit
-    @post = Post.find(@post_id)
+    @post = Post.find( @post_id )
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new( params[:post] )
 
     if @post.save
-      redirect_to(@post, :notice => 'Post was successfully created.')
+      redirect_to( @post, :notice => 'Post was successfully created.' )
     else
+      flash[:error] = 'Problem creating post. Please try again...'
       render :action => :new
+    end
+  end
+
+  def update
+    @post = Post.find( params[:id].split( '-' ).first )
+
+    if @post.update_attributes( params[:post] )
+      redirect_to( @post, :notice => 'Post was successfully updated.' )
+    else
+      flash[:error] = 'Post was not updated.'
+      render :action => :edit
     end
   end
 
   private
   def post_id
-    @post_id = params[:id].split('-').first
+    @post_id = params[:id].split( '-' ).first
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,7 +6,7 @@ describe PostsController do
     it 'returns http success' do
       get :index
       response.should be_success
-      assigns(:posts).should_not be_nil
+      assigns( :posts ).should_not be_nil
     end
   end
 
@@ -14,37 +14,55 @@ describe PostsController do
     it 'returns http success' do
       get :new
       response.should be_success
-      assigns(:post).should_not be_nil
+      assigns( :post ).should_not be_nil
     end
   end
 
   describe "GET 'show'" do
     it 'returns http success' do
-      get :show, :id => Factory(:post).to_param
+      get :show, :id => Factory( :post ).to_param
       response.should be_success
     end
   end
 
   describe "GET 'edit'" do
     it 'returns http success' do
-      @post = Factory(:post)
+      @post = Factory( :post )
       get :edit, :id => @post.to_param
       response.should be_success
-      assigns(:post).should_not be_nil
+      assigns( :post ).should_not be_nil
     end
   end
 
   describe '#create' do
-    describe 'with valid params' do
-      it 'succeeds' do
-        lambda do
-          post :create, :post => Factory.build(:post).as_json
-        end.should change(Post, :count).by(1)
+    it 'succeeds' do
+      lambda do
+        post :create, :post => Factory.build( :post ).as_json
+      end.should change( Post, :count ).by( 1 )
+      response.should be_redirect
+    end
+  end
+
+  describe '#update' do
+    def modifiy_params( hash )
+      hash.inject( {} ) do | memo, ( k,v ) |
+        if v.is_a?( Hash )
+          memo.merge!( k => modifiy_params( v ) )
+        else
+          if v.is_a?(String)
+            memo[k.to_sym] = v + ( k == :email_address ? '.modified' : ' (modified)' )
+          else
+            memo[k.to_sym] = v
+          end
+        end
+        memo
       end
     end
-
-    describe 'with invalid params' do
-
+    it 'succeeds' do
+      post_obj = Factory( :post )
+      updated_params = modifiy_params( post_obj.as_json )
+      put :update, :id => post_obj.to_param, :post => updated_params.as_json
+      response.should be_redirect
     end
   end
 end
