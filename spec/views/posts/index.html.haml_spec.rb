a1c7c5dc21b433815cf6d99417bbf81cd367659c
Girish Tryambake <gtryambake@host-154-7-0-10.sfoffice.verticalresponse.com>
2011-12-05 15:52:11 -0800

    specs modified for index page now passing

--- spec/views/posts/index.html.haml_spec.rb
+++ spec/views/posts/index.html.haml_spec.rb
@@ -7,17 +7,9 @@ describe 'posts/index.html.haml' do
     render
   end
 
-  describe 'form' do
+  describe 'page' do
     it 'has a title' do
-      rendered.should have_selector('div', :class => 'post') do |content|
-        content.css('h2').text.should == 'New Post'
-      end
-    end
-
-    it 'has a text box for title of the post' do
-      rendered.should have_selector('div', :class => 'sidebar') do |sidebar|
-        sidebar.should have_selector("input[name='post[title]']")
-      end
+      rendered.should have_selector('h1', :text => 'Post#index')
     end
   end
 end
