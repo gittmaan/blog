7b1e0be82ae88134e5b6326be30ccc295b8f0f25
gitman <https://github.com/gitman>
2011-12-17 13:18:35 -0800

    patch for specific env settings

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,1 +1,8 @@
+module Settings
+  def self.extended(base)
+    base.instance_variable_set "@_settings", Hash.new(File.join(Dir.pwd, "config", "settings.yml"), Rails.env)
+  end
+end
+
 Rails.extend Settings
+
